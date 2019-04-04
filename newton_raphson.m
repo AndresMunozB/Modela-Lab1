d0567026adc903b9b6044bef37bce8eaceebea21
Andrés Muñoz <andres.munoz.b@usach.cl>
2019-04-04 16:55:52 -0300

    ADD: Se agrega metodo de Newton Raphson

---
+++ newton_raphson.m
@@ -0,0 +1,12 @@
+function [raiz] = newton_raphson(poly, iter, tol, xi)
+    xrOld = xi;
+    xr = xrOld - (polyval(poly,xrOld)/polyval(polyder(poly),xrOld));
+    errorAbs = abs(xr-xrOld);
+    if(errorAbs < tol)
+        raiz = xr;
+    elseif (iter == 0)
+        raiz = xr;
+    else
+        raiz = newton_raphson(poly, iter-1, tol, xr);
+    end
+        
